13bd00b8f1819afd63464d3c100c9e699c7bbd1e
Phatthanan Phutong <funtskp22@gmail.com>
2022-04-22 23:38:40 +0700

    Change date to dateTime

--- node.js/hw1/server.js
+++ node.js/hw1/server.js
@@ -1,11 +1,11 @@
 const http = require('http')
 const name = 'Phatthanan Phutong'
-const date = new Date().toLocaleString()
+const dateTime = new Date().toLocaleString()
 const server = http.createServer((req,res) => {//create function
-    res.write(name + '\n' + date)
+    res.write(name + '\n' + dateTime)
     res.end() 
 })
 
 server.listen(2337, () =>{
-    console.log('Server Started \n' + name + date)
+    console.log('Server Started \n' + name + dateTime)
 })
